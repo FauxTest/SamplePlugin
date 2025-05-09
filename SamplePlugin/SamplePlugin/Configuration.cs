963631435610146e950d0308fb9f13018c6554c9
FauxTest <brandon.silvia@mac.com>
2025-05-09 18:03:41 -0400

    Add files via upload

---
+++ SamplePlugin/SamplePlugin/Configuration.cs
@@ -0,0 +1,19 @@
+using System;
+using Dalamud.Configuration;
+
+namespace SamplePlugin;
+
+[Serializable]
+public class Configuration : IPluginConfiguration
+{
+    public int Version { get; set; } = 0;
+
+    public bool IsConfigWindowMovable { get; set; } = true;
+    public bool SomePropertyToBeSavedAndWithADefault { get; set; } = true;
+
+    // the below exist just to make saving less cumbersome
+    public void Save()
+    {
+        Plugin.PluginInterface.SavePluginConfig(this);
+    }
+}
